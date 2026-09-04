351537efad0b73305811ce20b6a2731f38f2c8d4
4dri4anxD <mauricio.uavsystems@gmail.com>
2026-09-04 13:15:28 -0600

    chore: version bump to 3.2.6

--- RemoteIDModule/version.h
+++ RemoteIDModule/version.h
@@ -1,4 +1,15 @@
-#define FW_VERSION_MAJOR 1
-#define FW_VERSION_MINOR 14
+#define FW_VERSION_MAJOR 4
+#define FW_VERSION_MINOR 0
+#define SW_VERSION_LAST 0
+
+#if defined(BOARD_AURELIA_RID_S3)
+#define HW_VERSION_MAJOR 5
+#define HW_VERSION_MINOR 0
+#else
+#define HW_VERSION_MAJOR 2
+#define HW_VERSION_MINOR 1
+#endif
+
+
 
 #include "git-version.h"
