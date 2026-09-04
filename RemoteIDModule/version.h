869f6c179d71a0e901e4885b2426f419cb4b3ba7
4dri4anxD <mauricio.uavsystems@gmail.com>
2026-09-04 12:35:56 -0600

    boards: add board configuration and version info

--- RemoteIDModule/version.h
+++ RemoteIDModule/version.h
@@ -1,4 +1,15 @@
-#define FW_VERSION_MAJOR 1
-#define FW_VERSION_MINOR 14
+#define FW_VERSION_MAJOR 3
+#define FW_VERSION_MINOR 2
+#define SW_VERSION_LAST 6
+
+#if defined(BOARD_AURELIA_RID_S3)
+#define HW_VERSION_MAJOR 5
+#define HW_VERSION_MINOR 0
+#else
+#define HW_VERSION_MAJOR 2
+#define HW_VERSION_MINOR 1
+#endif
+
+
 
 #include "git-version.h"
